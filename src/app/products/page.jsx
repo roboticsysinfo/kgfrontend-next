bde0f6eca2475653d6165a98b93f3bcb543803ec
roboticsysinfo <roboticsysinfo@gmail.com>
2025-05-17 14:40:52 +0530

    product page suspense use

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,31 +1,27 @@
-// src/app/products/page.jsx
-
 import Breadcrumb from "@/components/Breadcrumb";
 import ClientLayoutWrapper from "@/components/ClientLayoutWrapper";
 import ShopSection from "@/components/ShopSection";
-
+import { Suspense } from "react";
 
 export async function generateMetadata() {
-
   return {
     title: "Shop Fresh Local Products From Kissan Growth",
-    description: "Browse and buy fresh, locally sourced products directly from farmers on Kissan Growth. Enjoy high-quality, sustainable produce delivered to your door. Shop now!",
-    keywords: "Fresh local products, Buy direct from farmers, Sustainable produce, Kissan Growth products, Online farm shopping, Shop fresh groceries, Local farm products, Support farmers, Fresh produce delivery"
+    description: "Browse and buy fresh, locally sourced products directly from farmers on Kissan Growth...",
+    keywords: "Fresh local products, Buy direct from farmers, Sustainable produce...",
   };
-
 }
 
 const page = async () => {
-
   return (
-
     <ClientLayoutWrapper>
       <Breadcrumb title={"Products"} />
-      <ShopSection />
+
+      {/* Suspense for client components inside ShopSection */}
+      <Suspense fallback={<div>Loading products...</div>}>
+        <ShopSection />
+      </Suspense>
     </ClientLayoutWrapper>
-
   );
-
 };
 
 export default page;
